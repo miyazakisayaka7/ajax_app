3193f96bde79ed1b86f40d21f15c2b8e55670b2d
miyazakisayaka7 <une_charrue@yahoo.co.jp>
2021-03-31 01:47:51 +0900

    コントローラーにリクエストを送信する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,19 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("../memo")  // 追記する
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    console.log("test;")
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+}
+
+
+
+
+
+window.addEventListener('load', post);
